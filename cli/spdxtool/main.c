2535b43879c8c9d440d887e22dd8403d62ca85ac
Elizabeth Kiara Regina Ashford <elizabeth.jennifer.myers@gmail.com>
2026-03-23 19:14:41 -0700

    spdxtool: fix leak check
    Signed-off-by: Elizabeth Kiara Regina Ashford <elizabeth.jennifer.myers@gmail.com>

--- cli/spdxtool/main.c
+++ cli/spdxtool/main.c
@@ -147,7 +147,7 @@ generate_spdx_package(pkgconf_client_t *client, pkgconf_pkg_t *pkg, void *ptr)
 	node = calloc(1, sizeof(pkgconf_node_t));
 	if (!node)
 	{
-		pkgconf_error(client, "Memory exhausted!");
+		pkgconf_error(client, "generate_spdx_package: out of memory");
 		return;
 	}
 
@@ -192,7 +192,7 @@ generate_spdx(pkgconf_client_t *client, pkgconf_pkg_t *world, const char *creati
 	}
 	else
 	{
-		pkgconf_error(client, "Could not serialize document");
+		pkgconf_error(client, "generate_spdx: couldn't serialize document");
 	}
 	spdxtool_serialize_value_free(root);
 	pkgconf_buffer_finalize(&buffer);
